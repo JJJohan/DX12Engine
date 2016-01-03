2186bfea22740d566715dae269f87609b4008e42
Johan Rensenbrink <admin@renscreations.com>
2016-01-03 22:09:09 +1000

    The command queue has been significantly refactored to improve parallelism as well as stability and performance.

--- Engine/Engine/Rendering/DX12/CommandQueue.cpp
+++ Engine/Engine/Rendering/DX12/CommandQueue.cpp
@@ -1,21 +1,198 @@
 #include "CommandQueue.h"
+#include <thread>
+#include "../../Utils/Logging.h"
+#include "../../Factory/Factory.h"
+#include "../../Utils/SystemInfo.h"
+#include <sstream>
 
 namespace Engine
 {
 	std::queue<std::function<void()>> CommandQueue::_tasks;
+	std::mutex CommandQueue::_releaseMutex;
+	bool CommandQueue::_releaseRequested = false;
+	std::vector<CommandThread*> CommandQueue::_commandThreads;
 
-	void CommandQueue::Enqueue(std::function<void()> task)
+	void CommandQueue::Enqueue(const std::function<void()>& task)
 	{
 		_tasks.push(task);
 	}
 
-	void CommandQueue::Process()
+	void CommandQueue::Release()
 	{
-		while (!_tasks.empty())
+		_releaseMutex.lock();
+		_releaseRequested = true;
+		_releaseMutex.unlock();
+
+		for (auto it = _commandThreads.begin(); it != _commandThreads.end(); ++it)
 		{
-			std::function<void()> task = _tasks.front();
-			task();
-			_tasks.pop();
+			CommandThread* commandThread = *it;
+			commandThread->Thread.join();
+			commandThread->CommandList->Release();
+			commandThread->CommandAllocator->Release();
+			delete commandThread;
 		}
 	}
+
+	void CommandQueue::RunTask(CommandThread* commandThread)
+	{
+		Factory::AssignCommandList(commandThread->CommandList);
+		ID3D12GraphicsCommandList* graphicsCmdList = static_cast<ID3D12GraphicsCommandList*>(commandThread->CommandList);
+
+		while (!_releaseRequested)
+		{
+			if (commandThread->Task == nullptr)
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				if (commandThread->TasksCompleted == 0)
+				{
+					commandThread->IdleTimer += 1.0f;
+					if (commandThread->IdleTimer > 2000.0f)
+					{
+						break;
+					}
+				}
+			}
+			else
+			{
+				commandThread->Task();
+
+				commandThread->Mutex.lock();
+				++commandThread->TasksCompleted;
+				commandThread->Available = true;
+				commandThread->Task = nullptr;
+				commandThread->Mutex.unlock();
+				commandThread->IdleTimer = 0.0f;
+			}
+		}
+
+		commandThread->Mutex.lock();
+		commandThread->IdleTimeOut = true;
+		commandThread->Mutex.unlock();
+	}
+
+	CommandThread* CommandQueue::GetAvailableThread()
+	{
+		for (auto it = _commandThreads.begin(); it != _commandThreads.end(); ++it)
+		{
+			CommandThread* commandThread = *it;
+			if (commandThread->Available)
+			{
+				return commandThread;
+			}
+		}
+
+		return nullptr;
+	}
+
+	std::vector<ID3D12CommandList*> CommandQueue::Process(ID3D12Device* device)
+	{
+		std::vector<ID3D12CommandList*> commandLists;
+
+		// Begin work load.
+		int taskCount = int(_tasks.size());
+		while (!_tasks.empty())
+		{
+			CommandThread* commandThread = GetAvailableThread();
+			if (commandThread == nullptr)
+			{
+				if (_commandThreads.size() < SystemInfo::GetCPUCores())
+				{
+					std::function<void()> task = _tasks.front();
+					_tasks.pop();
+
+					// Create the command list.
+					commandThread = new CommandThread;
+					commandThread->Available = false;
+					commandThread->TasksCompleted = 0;
+					commandThread->IdleTimeOut = false;
+					commandThread->Task = task;
+					commandThread->Open = true;
+
+					ID3D12CommandAllocator* commandAllocator;
+					ID3D12GraphicsCommandList* commandList;
+					LOGFAILEDCOM(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator)));
+					LOGFAILEDCOM(device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList)));
+					commandThread->CommandAllocator = commandAllocator;
+					commandThread->CommandList = commandList;
+					commandThread->Thread = std::thread(RunTask, commandThread);
+
+					_commandThreads.push_back(commandThread);
+				}
+				else
+				{
+					std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				}
+			}
+			else
+			{
+				std::function<void()> task = _tasks.front();
+				_tasks.pop();
+
+				if (!commandThread->Open)
+				{
+					// reset the command allocator and command list.
+					commandThread->CommandAllocator->Reset();
+					ID3D12GraphicsCommandList* graphicsCmdList = static_cast<ID3D12GraphicsCommandList*>(commandThread->CommandList);
+					graphicsCmdList->Reset(commandThread->CommandAllocator, nullptr);
+					commandThread->Open = true;
+				}
+
+				_releaseMutex.lock();
+				commandThread->Task = task;
+				commandThread->Available = false;
+				_releaseMutex.unlock();
+			}
+		}
+
+		// Wait for all tasks to complete.
+		int complete = 0;
+		while (complete != taskCount)
+		{
+			complete = 0;
+			for (auto it = _commandThreads.begin(); it != _commandThreads.end(); ++it)
+			{
+				complete += (*it)->TasksCompleted;
+			}
+		}
+
+		// Close command lists.
+		for (auto it = _commandThreads.begin(); it != _commandThreads.end(); ++it)
+		{
+			CommandThread* commandThread = *it;
+			if (commandThread->Open)
+			{
+				_releaseMutex.lock();
+				commandThread->TasksCompleted = 0;
+				commandThread->Available = true;
+				commandThread->Open = false;
+				_releaseMutex.unlock();
+
+				ID3D12GraphicsCommandList* graphicsCmdList = static_cast<ID3D12GraphicsCommandList*>(commandThread->CommandList);
+				graphicsCmdList->Close();
+				commandLists.push_back(commandThread->CommandList);
+			}
+		}
+
+		// Cleanup idle threads.
+		for (auto it = _commandThreads.begin(); it != _commandThreads.end(); ++it)
+		{
+			if ((*it)->IdleTimeOut)
+			{
+				std::stringstream ss;
+				ss << " Reduced command threads from " << _commandThreads.size() << " to " << _commandThreads.size() - 1 << ".";
+				Logging::Log(ss.str());
+
+				CommandThread* commandThread = *it;
+				commandThread->Thread.join();
+				commandThread->CommandList->Release();
+				commandThread->CommandAllocator->Release();
+				delete commandThread;
+				_commandThreads.erase(it);
+				break; // One at a time is fine.
+			}
+		}
+
+		return commandLists;
+	}
 }
+
--- Engine/Engine/Rendering/DX12/CommandQueue.h
+++ Engine/Engine/Rendering/DX12/CommandQueue.h
@@ -2,16 +2,41 @@
 
 #include <queue>
 #include <functional>
+#include <d3d12.h>
+#include <mutex>
 
 namespace Engine
 {
+	struct CommandThread
+	{
+		ID3D12CommandList* CommandList;
+		ID3D12CommandAllocator* CommandAllocator;
+		std::thread Thread;
+		std::mutex Mutex;
+		bool Available;
+		bool Open;
+		int TasksCompleted;
+		std::function<void()> Task;
+		float IdleTimer;
+		bool IdleTimeOut;
+	};
+
 	class CommandQueue
 	{
 	public:
-		static void Enqueue(std::function<void()> task);
-		static void Process();
+		static void Enqueue(const std::function<void()>& task);
+
+		static std::vector<ID3D12CommandList*> CommandQueue::Process(ID3D12Device* device);
+		static void Release();
 
 	private:
+		static void RunTask(CommandThread* commandThread);
+		static inline CommandThread* GetAvailableThread();
+
 		static std::queue<std::function<void()>> _tasks;
+		static std::vector<CommandThread*> _commandThreads;
+		static std::mutex _releaseMutex;
+		static bool _releaseRequested;
 	};
 }
+
