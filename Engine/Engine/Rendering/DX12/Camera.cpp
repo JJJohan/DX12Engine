6cdcf34b297d85242768923e5ced5a2237e4774c
Johan Rensenbrink <admin@renscreations.com>
2016-01-10 21:15:21 +1000

    Updated camera to use the new transform and constant buffer system.

--- Engine/Engine/Rendering/DX12/Camera.cpp
+++ Engine/Engine/Rendering/DX12/Camera.cpp
@@ -1,26 +1,12 @@
 #include "Camera.h"
-#include "../../Utils/Logging.h"
-#include "d3dx12.h"
+#include "../../Factory/ResourceFactory.h"
 #include "../../Input/Input.h"
 #include "../../Core/Time.h"
+#include "ConstantBuffer.h"
 #include <sstream>
 
 namespace Engine
 {
-	inline DirectX::XMVECTOR F2V(DirectX::XMFLOAT3 val)
-	{
-		return DirectX::XMLoadFloat3(&val);
-	}
-
-	inline DirectX::XMFLOAT3 V2F(DirectX::XMVECTOR vec)
-	{
-		DirectX::XMFLOAT3 val;
-		XMStoreFloat3(&val, vec);
-		return val;
-	}
-
-	XMVECTOR Camera::_up = F2V(XMFLOAT3(0.0f, 1.0f, 0.0f));
-
 	Camera* Camera::CreateCamera(ID3D12Device* device, const XMFLOAT4& screenRect, float fovInDegrees, float nearClip, float farClip)
 	{
 		Camera* camera = new Camera(device);
@@ -36,78 +22,73 @@ namespace Engine
 
 	Camera::Camera(ID3D12Device* device)
 		: _fov(90.0f)
-		  , _transformed(true)
 		  , _pDevice(device)
+		  , _pCbuffer(nullptr)
 	{
-		// Create constant buffer view descriptor heap
-		CD3DX12_RESOURCE_DESC constantBuffer = CD3DX12_RESOURCE_DESC::Buffer(sizeof XMFLOAT4X4 * 3);
-		LOGFAILEDCOM(
-			_pDevice->CreateCommittedResource(
-				&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
-			D3D12_HEAP_FLAG_NONE,
-			&constantBuffer,
-			D3D12_RESOURCE_STATE_GENERIC_READ,
-			nullptr,
-			IID_PPV_ARGS(&_cbuffer)));
-
-		_position = F2V(XMFLOAT3(0, 0, -1));
-		_rotation = XMQuaternionIdentity();
+		Transform.SetPosition(0.0f, 0.0f, -1.0f);
 
 		Input::RegisterKey('W', KeyHeld, [&]
 		                   {
 			                   float dt = Time::DeltaTime();
-			                   Move(XMFLOAT3(0.0f, 0.0f, 1.0f * dt), Local);
+			                   this->Transform.Move(Vector3(0.0f, 0.0f, 1.0f * dt), Local);
 		                   });
 
 		Input::RegisterKey('S', KeyHeld, [&]
 		                   {
 			                   float dt = Time::DeltaTime();
-			                   Move(XMFLOAT3(0.0f, 0.0f, -1.0f * dt), Local);
+			                   this->Transform.Move(Vector3(0.0f, 0.0f, -1.0f * dt), Local);
 		                   });
 
 		Input::RegisterKey('A', KeyHeld, [&]
 		                   {
 			                   float dt = Time::DeltaTime();
-			                   Move(XMFLOAT3(-1.0f * dt, 0.0f, 0.0f), Local);
+			                   this->Transform.Move(Vector3(-1.0f * dt, 0.0f, 0.0f), Local);
 		                   });
 
 		Input::RegisterKey('D', KeyHeld, [&]
 		                   {
 			                   float dt = Time::DeltaTime();
-			                   Move(XMFLOAT3(1.0f * dt, 0.0f, 0.0f), Local);
+			                   this->Transform.Move(Vector3(1.0f * dt, 0.0f, 0.0f), Local);
 		                   });
 
 		Input::RegisterMouseMoveEvent([&](long x, long y)
 			{
-				Rotate(float(y) / 10, float(x) / 10, 0.0f);
+				this->Transform.Rotate(float(y) / 10, float(x) / 10, 0.0f);
 			});
 
-		LOGFAILEDCOM(
-			_cbuffer->SetName(L"Camera Data"));
+		_pCbuffer = ResourceFactory::CreateConstantBuffer();
+		//_pCbuffer->GetResource()->SetName(L"Camera Data");
 	}
 
-	ID3D12Resource* Camera::GetCBuffer() const
+	ConstantBuffer* Camera::GetCBuffer() const
 	{
-		return _cbuffer.Get();
+		return _pCbuffer;
 	}
 
 	Camera::~Camera()
 	{
+		if (_pCbuffer != nullptr)
+		{
+			delete _pCbuffer;
+		}
+	}
+
+	void Camera::Bind(ID3D12GraphicsCommandList* commandList) const
+	{
+		_pCbuffer->Bind(commandList);
 	}
 
 	bool Camera::Update()
 	{
-		bool transformed = _transformed;
+		bool transformed = Transform.Moved;
 
-		if (_transformed)
+		if (Transform.Moved)
 		{
-			XMVECTOR forward = F2V(XMFLOAT3(0, 0, 1));
-
-			_rotation = XMQuaternionNormalize(_rotation);
-			XMVECTOR direction = XMVector3Rotate(forward, _rotation);
+			Transform.SetRotation(XMQuaternionNormalize(Transform.GetRotation()));
+			XMVECTOR direction = XMVector3Rotate(Vector3::Forward, Transform.GetRotation());
 
 			// Calculate view matrix
-			_view = XMMatrixLookToLH(_position, direction, _up);
+			_view = XMMatrixLookToLH(Transform.GetPosition(), direction, Vector3::Up);
 
 			// Calculate projection matrix
 			_projection = XMMatrixPerspectiveFovLH(_fov, _viewport.Width / _viewport.Height, 0.01f, 100.0f);
@@ -126,13 +107,12 @@ namespace Engine
 			XMStoreFloat4x4(&camData.proj, projT);
 			XMStoreFloat4x4(&camData.vp, vpT);
 
-			// Map data to cbuffer
-			UINT8* memPtr;
-			_cbuffer->Map(0, nullptr, reinterpret_cast<void**>(&memPtr));
-			memcpy(memPtr, &camData, sizeof(camData));
-			_cbuffer->Unmap(0, nullptr);
+			// Update the camera CBuffer.
+			_pCbuffer->SetMatrix("view", camData.view);
+			_pCbuffer->SetMatrix("proj", camData.proj);
+			_pCbuffer->SetMatrix("vp", camData.vp);
 
-			_transformed = false;
+			Transform.Moved = false;
 		}
 
 		return transformed;
@@ -153,90 +133,6 @@ namespace Engine
 		return _vp;
 	}
 
-	XMVECTOR Camera::GetPosition() const
-	{
-		return _position;
-	}
-
-	void Camera::SetPosition(XMVECTOR position)
-	{
-		_position = position;
-		_transformed = true;
-	}
-
-	void Camera::SetPosition(XMFLOAT3 position)
-	{
-		SetPosition(F2V(position));
-	}
-
-	XMVECTOR Camera::GetRotation() const
-	{
-		return _rotation;
-	}
-
-	XMFLOAT3 Camera::GetEulerAngles() const
-	{
-		XMFLOAT4 q;
-		XMStoreFloat4(&q, _rotation);
-
-		XMFLOAT3 angles;
-		angles.x = atan2(2 * q.y * q.w - 2 * q.x * q.z, 1 - 2 * (q.y * q.y) - 2 * (q.z * q.z));
-		angles.y = asin(2 * q.x * q.y + 2 * q.z * q.w);
-		angles.z = atan2(2 * q.x * q.w - 2 * q.y * q.z, 1 - 2 * (q.x * q.x) - 2 * (q.z * q.z));
-
-		return angles;
-	}
-
-	void Camera::SetEulerAngles(const XMFLOAT3& eulerAngles)
-	{
-		float pitch = XMConvertToRadians(eulerAngles.x);
-		float yaw = XMConvertToRadians(eulerAngles.y);
-		float roll = XMConvertToRadians(eulerAngles.z);
-
-		_rotation = XMQuaternionRotationRollPitchYaw(pitch, yaw, roll);
-		_transformed = true;
-	}
-
-	void Camera::SetEulerAngles(float pitch, float yaw, float roll)
-	{
-		SetEulerAngles(XMFLOAT3(pitch, yaw, roll));
-	}
-
-	void Camera::SetRotation(XMVECTOR rotation)
-	{
-		_rotation = rotation;
-		_transformed = true;
-	}
-
-	void Camera::Move(XMFLOAT3 translation, Space relativeTo)
-	{
-		XMVECTOR trans = F2V(translation);
-		if (relativeTo == Local)
-		{
-			trans = XMVector3Rotate(trans, _rotation);
-		}
-
-		_position = _position + trans;
-		_transformed = true;
-	}
-
-	void Camera::Rotate(const XMFLOAT3& eulerAngles)
-	{
-		float pitch = XMConvertToRadians(eulerAngles.x);
-		float yaw = XMConvertToRadians(eulerAngles.y);
-		float roll = XMConvertToRadians(eulerAngles.z);
-
-		XMVECTOR rotation = XMQuaternionRotationRollPitchYaw(pitch, yaw, roll);
-		_rotation = XMQuaternionMultiply(_rotation, rotation);
-
-		_transformed = true;
-	}
-
-	void Camera::Rotate(float pitch, float yaw, float roll)
-	{
-		Rotate(XMFLOAT3(pitch, yaw, roll));
-	}
-
 	float Camera::GetFOV() const
 	{
 		return XMConvertToDegrees(_fov);
@@ -245,7 +141,7 @@ namespace Engine
 	void Camera::SetFOV(float fov)
 	{
 		_fov = XMConvertToRadians(fov);
-		_transformed = true;
+		Transform.Moved = true;
 	}
 
 	D3D12_VIEWPORT& Camera::GetViewPort()
--- Engine/Engine/Rendering/DX12/Camera.h
+++ Engine/Engine/Rendering/DX12/Camera.h
@@ -2,37 +2,18 @@
 
 #include <d3d12.h>
 #include <DirectXMath.h>
-#include <wrl/client.h>
+#include "../../Data/Transform.h"
 
 using namespace DirectX;
-using namespace Microsoft::WRL;
 
 namespace Engine
 {
-	enum Space
-	{
-		World,
-		Local
-	};
+	class ConstantBuffer;
 
 	class Camera
 	{
 	public:
 		static Camera* CreateCamera(ID3D12Device* device, const XMFLOAT4& screenRect, float fovInDegrees, float nearClip, float farClip);
-
-		XMVECTOR GetPosition() const;
-		void SetPosition(XMFLOAT3 position);
-		void SetPosition(XMVECTOR position);
-
-		XMFLOAT3 GetEulerAngles() const;
-		void SetEulerAngles(float pitch, float yaw, float roll);
-		void SetEulerAngles(const XMFLOAT3& eulerAngles);
-		XMVECTOR GetRotation() const;
-		void SetRotation(XMVECTOR rotation);
-
-		void Move(XMFLOAT3 translation, Space relativeTo = World);
-		void Rotate(const XMFLOAT3& eulerAngles);
-		void Rotate(float pitch, float yaw, float roll);
 
 		float GetFOV() const;
 		void SetFOV(float fovInDegrees);
@@ -41,28 +22,26 @@ namespace Engine
 		XMMATRIX GetViewMatrix() const;
 		XMMATRIX GetProjectionMatrix() const;
 		XMMATRIX GetVPMatrix() const;
-		ID3D12Resource* GetCBuffer() const;
+		ConstantBuffer* GetCBuffer() const;
 
 		bool Update();
+		void Bind(ID3D12GraphicsCommandList* commandList) const;
 
 		~Camera();
+
+		Transform Transform;
+
 	private:
 		Camera(ID3D12Device* device);
 
-		XMVECTOR _position;
-		XMVECTOR _rotation;
-
 		D3D12_VIEWPORT _viewport;
 		float _fov;
-		bool _transformed;
 		ID3D12Device* _pDevice;
-		ComPtr<ID3D12Resource> _cbuffer;
+		ConstantBuffer* _pCbuffer;
 
 		XMMATRIX _view;
 		XMMATRIX _projection;
 		XMMATRIX _vp;
-
-		static XMVECTOR _up;
 
 		struct CameraData
 		{
