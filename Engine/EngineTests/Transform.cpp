e12121d8d491b1bb36c91096e28f7708ebf0d4ee
Johan Rensenbrink <admin@renscreations.com>
2016-01-24 21:49:20 +1000

    Added unit tests for Transform class.

--- Engine/EngineTests/Transform.cpp
+++ Engine/EngineTests/Transform.cpp
@@ -1,6 +1,3 @@
-#include "stdafx.h"
-#include "CppUnitTest.h"
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace DirectX;
 
@@ -9,9 +6,28 @@ namespace EngineTests
 	TEST_CLASS(TransformTests)
 	{
 	public:
-		TEST_METHOD(Transform)
+		TEST_METHOD(TransformMethods)
 		{
-			Assert::Fail(L"Transform tests not implemented yet.");
+			Engine::Transform transform;
+
+			transform.SetPosition(Engine::Vector3(1.0f, 2.0f, 3.0f));
+			Engine::Vector3 pos = transform.GetPosition();
+			Assert::AreEqual(1.0f, pos.X, 0.01f, L"Pos X did not equal input of 1.0f.");
+			Assert::AreEqual(2.0f, pos.Y, 0.01f, L"Pos Y did not equal input of 2.0f.");
+			Assert::AreEqual(3.0f, pos.Z, 0.01f, L"Pos Z did not equal input of 3.0f.");
+
+			transform.SetRotation(Engine::Quaternion(1.0f, 2.0f, 3.0f, 4.0f));
+			Engine::Quaternion rot = transform.GetRotation();
+			Assert::AreEqual(1.0f, rot.X, 0.01f, L"Rot X did not equal input of 1.0f.");
+			Assert::AreEqual(2.0f, rot.Y, 0.01f, L"Rot Y did not equal input of 2.0f.");
+			Assert::AreEqual(3.0f, rot.Z, 0.01f, L"Rot Z did not equal input of 3.0f.");
+			Assert::AreEqual(4.0f, rot.W, 0.01f, L"Rot W did not equal input of 4.0f.");
+
+			transform.SetScale(Engine::Vector3(1.0f, 2.0f, 3.0f));
+			Engine::Vector3 scale = transform.GetScale();
+			Assert::AreEqual(1.0f, scale.X, 0.01f, L"Scale X did not equal input of 1.0f.");
+			Assert::AreEqual(2.0f, scale.Y, 0.01f, L"Scale Y did not equal input of 2.0f.");
+			Assert::AreEqual(3.0f, scale.Z, 0.01f, L"Scale Z did not equal input of 3.0f.");
 		}
 	};
 }
