7c0a46501aeb6f031793aab067b1b07bb1555a07
Johan Rensenbrink <admin@renscreations.com>
2016-01-24 21:50:10 +1000

    Updated math header to have a Math namespace to reduce chance of name conflicts.

--- Engine/Engine/Utils/Math.h
+++ Engine/Engine/Utils/Math.h
@@ -4,27 +4,29 @@
 
 namespace Engine
 {
-	inline float Min(float a, float b)
+	namespace Math
 	{
-		// Branchless SSE min.
-		_mm_store_ss(&a, _mm_min_ss(_mm_set_ss(a), _mm_set_ss(b)));
-		return a;
-	}
+		inline float Min(float a, float b)
+		{
+			// Branchless SSE min.
+			_mm_store_ss(&a, _mm_min_ss(_mm_set_ss(a), _mm_set_ss(b)));
+			return a;
+		}
 
-	inline float Max(float a, float b)
-	{
-		// Branchless SSE max.
-		_mm_store_ss(&a, _mm_max_ss(_mm_set_ss(a), _mm_set_ss(b)));
-		return a;
-	}
+		inline float Max(float a, float b)
+		{
+			// Branchless SSE max.
+			_mm_store_ss(&a, _mm_max_ss(_mm_set_ss(a), _mm_set_ss(b)));
+			return a;
+		}
 
-	inline float Clamp(float val, float minval, float maxval)
-	{
-		// Branchless SSE clamp.
-		// return minss( maxss(val,minval), maxval );
+		inline float Clamp(float val, float minval, float maxval)
+		{
+			// Branchless SSE clamp.
+			// return minss( maxss(val,minval), maxval );
 
-		_mm_store_ss(&val, _mm_min_ss(_mm_max_ss(_mm_set_ss(val), _mm_set_ss(minval)), _mm_set_ss(maxval)));
-		return val;
+			_mm_store_ss(&val, _mm_min_ss(_mm_max_ss(_mm_set_ss(val), _mm_set_ss(minval)), _mm_set_ss(maxval)));
+			return val;
+		}
 	}
 }
-
