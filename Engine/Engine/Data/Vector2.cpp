fcc8476a8c496e60fb882fb180315ea4e89afd57
Johan Rensenbrink <admin@renscreations.com>
2016-01-24 21:48:48 +1000

    Updated Vector2 class to be up to date with the other vector classes.

--- Engine/Engine/Data/Vector2.cpp
+++ Engine/Engine/Data/Vector2.cpp
@@ -2,28 +2,53 @@ using namespace DirectX;
 
 namespace Engine
 {
-	const Vector2 Vector2::One = Vector2(1, 1);
-	const Vector2 Vector2::Zero = Vector2(0, 0);
+	const Vector2& Vector2::One = Vector2(1, 1);
+	const Vector2& Vector2::Zero = Vector2(0, 0);
+	const Vector2& Vector2::Up = Vector2(0, 1);
+	const Vector2& Vector2::Down = Vector2(0, -1);
+	const Vector2& Vector2::Left = Vector2(-1, 0);
+	const Vector2& Vector2::Right = Vector2(1, 0);
 
 	Vector2::Vector2()
-		: X(0.0f)
-		  , Y(0.0f)
+		: X(_dxVector.m128_f32[0])
+		, Y(_dxVector.m128_f32[1])
 	{
 		_dxVector = XMVectorZero();
 	}
 
-	Vector2::Vector2(int x, int y)
-		: X(float(x))
-		  , Y(float(y))
-	{
-		_dxVector = XMVectorSetInt(x, y, 0, 0);
-	}
-
 	Vector2::Vector2(float x, float y)
-		: X(x)
-		  , Y(y)
+		: X(_dxVector.m128_f32[0])
+		, Y(_dxVector.m128_f32[1])
 	{
 		_dxVector = XMVectorSet(x, y, 0.0f, 0.0f);
+	}
+
+	Vector2::Vector2(const XMVECTOR& vector)
+		: X(_dxVector.m128_f32[0])
+		, Y(_dxVector.m128_f32[1])
+	{
+		_dxVector = vector;
+	}
+
+	Vector2::Vector2(const XMFLOAT2& vector)
+		: X(_dxVector.m128_f32[0])
+		, Y(_dxVector.m128_f32[1])
+	{
+		_dxVector = XMLoadFloat2(&vector);
+	}
+
+	Vector2::Vector2(int x, int y)
+		: X(_dxVector.m128_f32[0])
+		, Y(_dxVector.m128_f32[1])
+	{
+		_dxVector = XMVectorSet(float(x), float(y), 0.0f, 0.0f);
+	}
+
+	Vector2::Vector2(const Vector2& rhs)
+		: X(_dxVector.m128_f32[0])
+		, Y(_dxVector.m128_f32[1])
+	{
+		_dxVector = XMVectorSet(rhs.X, rhs.Y, 0.0f, 0.0f);
 	}
 
 	Vector2 Vector2::operator+(const Vector2& rhs) const
@@ -50,6 +75,12 @@ namespace Engine
 		return Vector2(v.m128_f32[0], v.m128_f32[1]);
 	}
 
+	void Vector2::operator=(const Vector2& rhs)
+	{
+		XMVECTOR v = XMVectorSet(rhs.X, rhs.Y, 0.0f, 0.0f);
+		_dxVector = v;
+	}
+
 	Vector2 Vector2::operator*(const float& rhs) const
 	{
 		XMVECTOR v = XMVectorScale(_dxVector, rhs);
@@ -65,7 +96,6 @@ namespace Engine
 	Vector2& Vector2::operator+=(const Vector2& rhs)
 	{
 		_dxVector = XMVectorAdd(_dxVector, rhs._dxVector);
-		Sync();
 
 		return *this;
 	}
@@ -73,7 +103,6 @@ namespace Engine
 	Vector2& Vector2::operator*=(const Vector2& rhs)
 	{
 		_dxVector = XMVectorMultiply(_dxVector, rhs._dxVector);
-		Sync();
 
 		return *this;
 	}
@@ -81,7 +110,6 @@ namespace Engine
 	Vector2& Vector2::operator/=(const Vector2& rhs)
 	{
 		_dxVector = XMVectorDivide(_dxVector, rhs._dxVector);
-		Sync();
 
 		return *this;
 	}
@@ -89,36 +117,33 @@ namespace Engine
 	Vector2& Vector2::operator*=(const float& rhs)
 	{
 		_dxVector = XMVectorScale(_dxVector, rhs);
-		Sync();
 
 		return *this;
+	}
+
+	Vector2::operator DirectX::XMFLOAT2() const
+	{
+		XMFLOAT2 vec;
+		XMStoreFloat2(&vec, _dxVector);
+		return vec;
+	}
+
+	Vector2::operator DirectX::XMVECTOR() const
+	{
+		return _dxVector;
 	}
 
 	Vector2& Vector2::operator/=(const float& rhs)
 	{
 		_dxVector = XMVectorScale(_dxVector, 1.0f / rhs);
-		Sync();
 
 		return *this;
-	}
-
-	DirectX::XMVECTOR Vector2::GetXMVector() const
-	{
-		return _dxVector;
 	}
 
 	Vector2& Vector2::operator-=(const Vector2& rhs)
 	{
 		_dxVector = XMVectorSubtract(_dxVector, rhs._dxVector);
-		Sync();
 
 		return *this;
 	}
-
-	inline void Vector2::Sync()
-	{
-		X = _dxVector.m128_f32[0];
-		Y = _dxVector.m128_f32[1];
-	}
 }
-
--- Engine/Engine/Data/Vector2.h
+++ Engine/Engine/Data/Vector2.h
@@ -8,8 +8,11 @@ namespace Engine
 	{
 	public:
 		ENGINE_API Vector2();
+		ENGINE_API Vector2(const Vector2& rhs);
 		ENGINE_API Vector2(float x, float y);
 		ENGINE_API Vector2(int x, int y);
+		ENGINE_API Vector2(const DirectX::XMVECTOR& vector);
+		ENGINE_API Vector2(const DirectX::XMFLOAT2& vector);
 
 		ENGINE_API Vector2 operator+(const Vector2& rhs) const;
 		ENGINE_API Vector2 operator*(const Vector2& rhs) const;
@@ -17,6 +20,7 @@ namespace Engine
 		ENGINE_API Vector2 operator/(const Vector2& rhs) const;
 		ENGINE_API Vector2 operator*(const float& rhs) const;
 		ENGINE_API Vector2 operator/(const float& rhs) const;
+		ENGINE_API void operator=(const Vector2& rhs);
 
 		ENGINE_API Vector2& operator+=(const Vector2& rhs);
 		ENGINE_API Vector2& operator*=(const Vector2& rhs);
@@ -25,17 +29,21 @@ namespace Engine
 		ENGINE_API Vector2& operator*=(const float& rhs);
 		ENGINE_API Vector2& operator/=(const float& rhs);
 
-		ENGINE_API DirectX::XMVECTOR GetXMVector() const;
+		ENGINE_API operator DirectX::XMFLOAT2() const;
+		ENGINE_API operator DirectX::XMVECTOR() const;
 
-		static ENGINE_API const Vector2 Zero;
-		static ENGINE_API const Vector2 One;
+		static ENGINE_API const Vector2& Zero;
+		static ENGINE_API const Vector2& One;
+		static ENGINE_API const Vector2& Up;
+		static ENGINE_API const Vector2& Left;
+		static ENGINE_API const Vector2& Right;
+		static ENGINE_API const Vector2& Down;
 
-		float X;
-		float Y;
+		float& X;
+		float& Y;
 
 	private:
 		DirectX::XMVECTOR _dxVector;
-		inline void Sync();
 	};
 }
 
