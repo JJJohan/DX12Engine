08625dbfbc1d4a733dc8a2a963f91d7da4c1c763
Johan Rensenbrink <admin@renscreations.com>
2016-01-24 21:49:31 +1000

    Added unit tests for Colour class.

--- Engine/EngineTests/Colour.cpp
+++ Engine/EngineTests/Colour.cpp
@@ -1,6 +1,3 @@
-#include "stdafx.h"
-#include "CppUnitTest.h"
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace DirectX;
 
@@ -11,7 +8,26 @@ namespace EngineTests
 	public:
 		TEST_METHOD(ColourClamping)
 		{
-			Assert::Fail(L"Colour operations not implemented yet.");
+			Engine::Colour col;
+			col = col + col.White;
+			Assert::AreEqual(1.0f, col.GetRed(), 0.01f, L"Colour R value is not 1.0f.");
+			Assert::AreEqual(1.0f, col.GetGreen(), 0.01f, L"Colour G value is not 1.0f.");
+			Assert::AreEqual(1.0f, col.GetBlue(), 0.01f, L"Colour B value is not 1.0f.");
+			Assert::AreEqual(1.0f, col.GetAlpha(), 0.01f, L"Colour A value is not 1.0f.");
+
+			col = Engine::Colour::White;
+			col = col - Engine::Colour(10.0f, 10.0f, 10.0f);
+			col = col + Engine::Colour(0.1f, 0.1f, 0.1f);
+			Assert::AreEqual(0.1f, col.GetRed(), 0.01f, L"Colour R value is not 0.1f.");
+			Assert::AreEqual(0.1f, col.GetGreen(), 0.01f, L"Colour G value is not 0.1f.");
+			Assert::AreEqual(0.1f, col.GetBlue(), 0.01f, L"Colour B value is not 0.1f.");
+			Assert::AreEqual(1.0f, col.GetAlpha(), 0.01f, L"Colour A value is not 1.0f.");
+
+			Engine::Colour col2 = Engine::Colour(-3.5f, 5.0f, .0123f);
+			Assert::AreEqual(0.0f, col2.GetRed(), 0.01f, L"Colour R value is not 0.0f.");
+			Assert::AreEqual(1.0f, col2.GetGreen(), 0.01f, L"Colour G value is not 1.0f.");
+			Assert::AreEqual(0.0123f, col2.GetBlue(), 0.01f, L"Colour B value is not 0.0123f.");
+			Assert::AreEqual(1.0f, col2.GetAlpha(), 0.01f, L"Colour A value is not 1.0f.");
 		}
 	};
 }
